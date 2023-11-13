7db2b0a97c7549cf1cca4617d92911122ea10b55
santhoshbollepalli <148257805+santhoshbollepalli@users.noreply.github.com>
2023-11-13 12:28:18 +0530

    Add files via upload

---
+++ src/Areaofrecatngle.java
@@ -0,0 +1,24 @@
+package com.santhosh.basics;
+
+import java.util.Scanner;
+
+public class Areaofrecatngle 
+{
+	Scanner sc= new Scanner(System.in);
+	void meth2()
+	{
+		int length,breadth, area;
+		System.out.println("Enter the length of the rectangle:");
+		length=sc.nextInt();
+		System.out.println("Enter the breadth of the rectangle:");
+		breadth=sc.nextInt();
+		area= length*breadth;
+		System.out.println("Area of the rectangle:"+area);
+	}
+	public static void main(String[] args) 
+	{
+		Areaofrecatngle bobj=new Areaofrecatngle();
+		bobj.meth2();
+	}
+
+}
