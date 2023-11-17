d185296f6b7584d7a165fb13d7d568fd69db8e7b
santhoshbollepalli <santhosh.b567@gmail.com>
2023-11-17 16:23:04 +0530

    commit 1

---
+++ src/pack1/Areaoftriangle.java
@@ -0,0 +1,25 @@
+package com.santhosh.basics.pack1;
+
+import java.util.Scanner;
+
+public class Areaoftriangle 
+{
+     Scanner sc= new Scanner(System.in);
+       void meth3() 
+       {
+    	int base, height, area;
+    	System.out.println("Enter the base of the triangle:");
+   		base=sc.nextInt();
+   		System.out.println("Enter the height of the triangle:");
+   		height=sc.nextInt();
+   		area= ((base*height)/2);// Area of triangle = 1/2(b*h)//
+   		System.out.println("Area of the triangle:"+area);
+       }
+       
+    	   public static void main(String[] args) 
+    	   { 
+    		   Areaoftriangle cobj=new Areaoftriangle();
+    		   cobj.meth3();
+       }
+
+}
